6800670b44820e66927f58cf049c760ddc11cbad
Michel Laporte <michlaporte15@gmail.com>
2020-07-24 12:45:46 -0300

    module 4, SC

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,6 +4,7 @@ import sys
 # python .\ls8.py examples\print8.ls8
 # python .\ls8.py examples\mult.ls8
 # python .\ls8.py examples\stack.ls8
+# python .\ls8.py examples\sctest.ls8
 
 """Operations table."""
 
@@ -27,8 +28,8 @@ import sys
 # SHR  = 0b10101101                                                            DONE
 
 # PC Mutators
-# CALL = 0b01010000                                                            #>>> TODO
-# RET  = 0b00010001                                                            #>>> TODO
+# CALL = 0b01010000                                                            DONE
+# RET  = 0b00010001                                                            DONE
 
 # INT  = 0b01010010                                                            #>>>
 # IRET = 0b00010011                                                            #>>>
@@ -42,7 +43,7 @@ import sys
 # JGE  = 0b01011010                                                            #>>>
 
 # Other
-# NOP  = 0b00000000                                                            #>>>
+# NOP  = 0b00000000
 
 # HLT  = 0b00000001                                                            DONE
 
@@ -57,10 +58,7 @@ import sys
 # PRN  = 0b01000111                                                            DONE
 # PRA  = 0b01001000                                                            #>>>
 
-
 """CPU functionality."""
-
-import sys
 
 class CPU:
     """Main CPU class."""
@@ -83,18 +81,18 @@ class CPU:
         
     def configure_branchtable(self):
         self.branchtable = {}
-        # self.branchtable[0b01010000] = self.handle_CALL                      #>>> TODO
-        # self.branchtable[0b00010001] = self.handle_RET                       #>>> TODO
+        self.branchtable[0b01010000] = self.handle_CALL
+        self.branchtable[0b00010001] = self.handle_RET
         # self.branchtable[0b01010010] = self.handle_INT                       #>>> TODO
         # self.branchtable[0b00010011] = self.handle_IRET                      #>>> TODO
-        # self.branchtable[0b01010100] = self.handle_JMP                       #>>> TODO
-        # self.branchtable[0b01010101] = self.handle_JEQ                       #>>> TODO
-        # self.branchtable[0b01010110] = self.handle_JNE                       #>>> TODO
-        # self.branchtable[0b01010111] = self.handle_JGT                       #>>> TODO
-        # self.branchtable[0b01011000] = self.handle_JLT                       #>>> TODO
-        # self.branchtable[0b01011001] = self.handle_JLE                       #>>> TODO
-        # self.branchtable[0b01011010] = self.handle_JGE                       #>>> TODO
-        # self.branchtable[0b00000000] = self.handle_NOP                       #>>> TODO
+        self.branchtable[0b01010100] = self.handle_JMP
+        self.branchtable[0b01010101] = self.handle_JEQ
+        self.branchtable[0b01010110] = self.handle_JNE
+        self.branchtable[0b01010111] = self.handle_JGT
+        self.branchtable[0b01011000] = self.handle_JLT
+        self.branchtable[0b01011001] = self.handle_JLE
+        self.branchtable[0b01011010] = self.handle_JGE
+        self.branchtable[0b00000000] = self.handle_NOP
         self.branchtable[0b00000001] = self.handle_HLT
         self.branchtable[0b10000010] = self.handle_LDI
         # self.branchtable[0b10000011] = self.handle_LD                        #>>> TODO
@@ -105,29 +103,68 @@ class CPU:
         # self.branchtable[0b01001000] = self.handle_PRA                       #>>> TODO
     
     # Branchtable operations
-    def handle_CALL(self):                                                     #>>> TODO
-        pass
-    def handle_RET(self):                                                      #>>> TODO
-        pass 
+    def handle_CALL(self, reg_num):
+        """Calls a subroutine at the address stored in reg_a."""
+        print(f"Handling CALL operation!")
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.PC+2
+        self.PC = self.reg[reg_num]
+    def handle_RET(self):
+        """Return from subroutine."""
+        print(f"Handling RET operation!\n")
+        self.PC = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1 
     def handle_INT(self):                                                      #>>> TODO
         pass 
     def handle_IRET(self):                                                     #>>> TODO
         pass
-    def handle_JMP(self):                                                      #>>> TODO
-        pass 
-    def handle_JEQ(self):                                                      #>>> TODO
-        pass 
-    def handle_JNE(self):                                                      #>>> TODO
-        pass 
-    def handle_JGT(self):                                                      #>>> TODO
-        pass 
-    def handle_JLT(self):                                                      #>>> TODO
-        pass 
-    def handle_JLE(self):                                                      #>>> TODO
-        pass 
-    def handle_JGE(self):                                                      #>>> TODO
-        pass 
-    def handle_NOP(self):                                                      #>>> TODO
+    def handle_JMP(self, reg_num): # 0b01010100 <NOTE>
+        """Jump to the address stored in the given register."""                # 00000LGE (Less-than, Greater-than, Equal)
+        print(f"Moving. JMP!\n")
+        self.PC = self.reg[reg_num]
+    def handle_JEQ(self, reg_num): # 0b01010101 <NOTE>
+        """If E flag is True, jump to the address stored in the given register."""
+        print(f"Moving. JEQ!\n") 
+        if (self.FL & 0b00000001) > 0:
+            self.PC = self.reg[reg_num]
+        else:
+            self.PC += 2
+    def handle_JNE(self, reg_num): # 0b01010110 <NOTE>
+        """If E flag is False, jump to the address stored in the given register."""
+        print(f"Moving. JNE!\n") 
+        if (self.FL & 0b00000001) == 0:
+            self.PC = self.reg[reg_num]
+        else:
+            self.PC += 2
+    def handle_JGT(self, reg_num): # 0b01010111
+        """If G flag is False, jump to the address stored in the given register."""
+        print(f"Moving. JGT!\n") 
+        if (self.FL & 0b00000010) == 0:
+            self.PC = self.reg[reg_num]
+        else:
+            self.PC += 2
+    def handle_JLT(self, reg_num): # 0b01011000
+        """If L flag is True, jump to the address stored in the given register."""
+        print(f"Moving. JLT!\n") 
+        if (self.FL & 0b00000100) > 0:
+            self.PC = self.reg[reg_num]
+        else:
+            self.PC += 2
+    def handle_JLE(self, reg_num): # 0b01011001
+        """If L OR E flags are True, jump to then adddress in the given register."""
+        print(f"Moving. JLE!\n") 
+        if (self.FL & 0b00000101) > 0:
+            self.PC = self.reg[reg_num]
+        else:
+            self.PC += 2
+    def handle_JGE(self, reg_num): # 0b01011010
+        """If G OR E flags are True, jump to the address stored in the given register."""
+        print(f"Moving. JGE!\n") 
+        if (self.FL & 0b00000011) > 0:
+            self.PC = self.reg[reg_num]
+        else:
+            self.PC += 2
+    def handle_NOP(self):
         pass 
     def handle_HLT(self):
         print("Handling HLT operation!")
@@ -150,7 +187,7 @@ class CPU:
         self.reg[reg_num] = self.ram_read(self.reg[self.SP+1])
         self.SP += 1
     def handle_PRN(self, reg_num):
-        print(f"Handling PRN operation! R{reg_num} >>> {self.reg[reg_num]}\n")
+        print(f"Handling PRN operation! R{reg_num}      >>> {self.reg[reg_num]}\n")
     def handle_PRA(self):                                                      #>>> TODO
         pass
         
@@ -219,81 +256,68 @@ class CPU:
             """Add the value from reg_a and reg_b, and store the result into reg_a"""
             print(f"ADD R0,({self.reg[0]}+{self.reg[1]})\n")
             self.reg[reg_a] += self.reg[reg_b]
-
         elif op == 0b10100001:
             """Substract the value from reg_a and reg_b, and store the result into reg_a"""
             print(f"SUB R0,({self.reg[0]}-{self.reg[1]})\n")
             self.reg[reg_a] -= self.reg[reg_b]
-
         elif op == 0b10100010:
             """Multiplies the value from reg_a and reg_b, and store the result into reg_a"""
             print(f"MUL R0,({self.reg[0]}*{self.reg[1]})\n")
             self.reg[reg_a] *= self.reg[reg_b]
-
         elif op == 0b10100011:
             """Divides the value from reg_a and reg_b, and store the result into reg_a"""
             print(f"DIV R0,({self.reg[0]}/{self.reg[1]})\n")
             self.reg[reg_a] /= self.reg[reg_b]
-
         elif op == 0b10100100:
             """Divides the value from reg_a and reg_b, and store the remainder into reg_a"""
             print(f"MOD R0,({self.reg[0]}%{self.reg[1]})\n")
             self.reg[reg_a] %= self.reg[reg_b]
-
         elif op == 0b01100101:
             """Increments the given register by 1"""
             print(f"INC R0,({self.reg[0]}+1)\n")
             self.reg[reg_a] += 1
-
         elif op == 0b01100110:
             """Decreases the given register by 1"""
             print(f"DEC R0,({self.reg[0]}-1)\n")
             self.reg[reg_a] -= 1
-
-        elif op == "0b10100111": # 0b00000LGE
+        elif op == 0b10100111: # 0b00000LGE <NOTE>
             """Compares the values of reg_a and reg_b."""
+            self.FL = 0b00000000
             if self.reg[reg_a] == self.reg[reg_b]:
-                print(f"CMP R0,{self.reg[0]} == R1,{self.reg[1]}")
-                self.FL = 0b00000001
+                self.FL += 0b00000001
+                print(f"Handling CMP! R0,{self.reg[0]} == R1,{self.reg[1]}\n0b{self.FL:b}\n")
             if self.reg[reg_a] < self.reg[reg_b]:
-                print(f"CMP R0,{self.reg[0]} < R1,{self.reg[1]}")
-                self.FL = 0b00000010
+                self.FL += 0b00000010
+                print(f"Handling CMP! R0,{self.reg[0]} < R1,{self.reg[1]}\n0b{self.FL:b}\n")
             if self.reg[reg_a] > self.reg[reg_b]:
-                print(f"CMP R0,{self.reg[0]} > R1,{self.reg[1]}")
-                self.FL = 0b00000100
-
+                self.FL += 0b00000100
+                print(f"Handling CMP! R0,{self.reg[0]} > R1,{self.reg[1]}\n0b{self.FL:b}\n")
         elif op == 0b10101000:
             """Bitwise-AND from reg_a and reg_b, and store the result into reg_a"""
             print(f"AND\n")
             self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
-
         elif op == 0b01101001:
             """Bitwise-NOT from reg_a and reg_b, and store the result into reg_a"""
             print(f"NOT\n")
             self.reg[reg_a] = ~ self.reg[reg_a]
-
         elif op == 0b10101010:
             """Bitwise-OR from reg_a and reg_b, and store the result into reg_a"""
             print(f"OR\n")
             self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
-
         elif op == 0b10101011:
             """Bitwise-XOR from reg_a and reg_b, and store the result into reg_a"""
             print(f"XOR\n")
             self.reg[reg_a] = self.reg[reg_a] ^ self.reg[reg_b]
-
         elif op == 0b10101100:
             """Shift the value in reg_a left by the number of bits specified in reg_b, filling the low bits with 0."""
             print(f"SHL\n")
             self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
-
         elif op == 0b10101101:
             """Shift the value in reg_a right by the number of bits specified in reg_b, filling the high bits with 0."""
             print(f"SHR\n")
             self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
-
         else:
-            raise Exception("Unsupported ALU operation")
+            raise Exception("Unknown ALU operation.")
 
 ###
 ###
@@ -341,16 +365,19 @@ class CPU:
                 self.branchtable[IR](reg_a)
                 self.PC += 2
             elif IR == 0b10100000: # ADD
-                self.alu("ADD", reg_a, reg_b)
+                self.alu(0b10100000, reg_a, reg_b)
                 self.PC += 3
             elif IR == 0b10100001: # SUB
-                self.alu("SUB", reg_a, reg_b)
+                self.alu(0b10100001, reg_a, reg_b)
                 self.PC += 3
             elif IR == 0b10100010: # MUL
-                self.alu("MUL", reg_a, reg_b)
+                self.alu(0b10100010, reg_a, reg_b)
                 self.PC += 3
             elif IR == 0b10100011: # DIV
-                self.alu("DIV", reg_a, reg_b)
+                self.alu(0b10100011, reg_a, reg_b)
+                self.PC += 3
+            elif IR == 0b10100111: # CMP <NOTE>
+                self.alu(0b10100111, reg_a, reg_b)
                 self.PC += 3
             elif IR == 0b01000101: # PUSH
                 self.branchtable[IR](reg_a)
@@ -359,8 +386,24 @@ class CPU:
                 self.branchtable[IR](reg_a)
                 self.PC += 2
             elif IR == 0b01010000: # CALL
-                self.branchtable[IR]()
+                self.branchtable[IR](reg_a)
             elif IR == 0b00010001: # RET
+                self.branchtable[IR]()
+            elif IR == 0b01010100: # JMP
+                self.branchtable[IR](reg_a) # <NOTE>
+            elif IR == 0b01010101: # JEQ
+                self.branchtable[IR](reg_a) # <NOTE>
+            elif IR == 0b01010110: # JNE
+                self.branchtable[IR](reg_a) # <NOTE>
+            elif IR == 0b01010111: # JGT
+                self.branchtable[IR](reg_a)
+            elif IR == 0b01011000: # JLT
+                self.branchtable[IR](reg_a)
+            elif IR == 0b01011001: # JLE
+                self.branchtable[IR](reg_a)
+            elif IR == 0b01011010: # JGE
+                self.branchtable[IR](reg_a)
+            elif IR == 0b00000000: # NOP
                 self.branchtable[IR]()
             else:
                 print(f"Unkown instruction {IR}")
